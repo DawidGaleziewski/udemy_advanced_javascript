2f86f4b6d5d2e1631cb319d9fd681dab83b573f7
Dawid <dawidgalsino@vp.pl>
2020-02-27 21:42:41 +0100

    update: learned more on function scopes and scope chains

--- 2_js_fundation-2/7_scope_chain.js
+++ 2_js_fundation-2/7_scope_chain.js
@@ -16,3 +16,42 @@ function globalOne() {
 }
 
 globalOne();
+
+var globalVar = 'globalVar';
+function tier3() {
+  var a = 'a';
+  // console.log(b); // Does not have access to variables inside tier2
+
+  return function tier2() {
+    var b = 'b';
+
+    return function tier1() {
+      console.log(b, a, globalVar); //Has access to all variables above
+      return 'tier1 string';
+    };
+  };
+}
+console.log(tier3()()());
+
+// Local scope is accessed to the function it was defined in and functions inside it. It is not accessible to functions that are parents of this function
+
+// ! eval and with change how scopes work internally. this can cause issues with optimizing for engine
+
+// [[scope]] property can be accessed on window -> function name -> [[Scopes]]
+
+// !leaking global scopes
+// var taptap
+function leak() {
+  taptap = 'leaking'; //if we do not declare variable in local scope. Engine goes up searching for the variable up to global scope. And then as it cant find it it creates the variable there
+  // ! to avoid this we use 'use strict'
+  return taptap;
+}
+
+// adding name for a function declaration!
+var pobiteGary = function takNieWolno() {
+  takNieWolno(); // We can only acces it from itself
+  return 'heyhey';
+};
+
+pobiteGary(); //We can run the name of variable
+takNieWolno(); //We cannot run it by function name. This is due to the fact that this function will be closed in its own scope
